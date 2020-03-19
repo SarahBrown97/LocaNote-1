0a0795a03aa729cd4a7ab16620862b9d3fe022cc
Lavish0202 <47317049+Lavish0202@users.noreply.github.com>
2020-03-19 18:52:38 -0400

    This is LocaNote

---
+++ app/src/main/java/com/example/locanote/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.locanote;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.Requirement;
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory;
+import com.estimote.proximity_sdk.api.EstimoteCloudCredentials;
+import com.estimote.proximity_sdk.api.ProximityObserver;
+import com.estimote.proximity_sdk.api.ProximityObserverBuilder;
+import com.estimote.proximity_sdk.api.ProximityZone;
+import com.estimote.proximity_sdk.api.ProximityZoneBuilder;
+import com.estimote.proximity_sdk.api.ProximityZoneContext;
+import com.suke.widget.SwitchButton;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
+import kotlin.jvm.functions.Function1;
+
+public class MainActivity extends AppCompatActivity {
+
+   SwitchButton switchButton;
+   SharedPreferences.Editor prefeditor;
+   SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        switchButton=findViewById(R.id.swithButton);
+        prefeditor=PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit();
+        preferences=PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        switchButton.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(MainActivity.this,"App is Enabled",Toast.LENGTH_SHORT).show();
+
+                    prefeditor.putString("checked","True");
+                    prefeditor.apply();
+                    Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Disabled",Toast.LENGTH_SHORT).show();
+                    prefeditor.putString("checked","False");
+                    prefeditor.apply();
+                }
+
+            }
+        });
+        if(preferences.getString("checked","no").equals("True")){
+            switchButton.setChecked(true);
+        }
+        else{
+            switchButton.setChecked(false);
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.locanote, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.homebtn:
+                Intent intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+        //Rest of the functionality to be added;
+
+
+    }
+}
